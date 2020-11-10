bc5f2705fc180015e158d58c90894d342e780961
mokainos <72137340+mokainos@users.noreply.github.com>
2020-11-10 17:05:30 +0000

    lowercase

--- frontdoor.tf
+++ frontdoor.tf
@@ -59,7 +59,7 @@ resource "azurerm_frontdoor" "main" {
       name                                    = host.value["name"]
       host_name                               = host.value["custom_domain"]
       custom_https_provisioning_enabled       = lookup(host.value, "enable_ssl", true)
-      web_application_firewall_policy_link_id = "/subscriptions/${var.subscription_id}/resourcegroups/${var.resource_group}/providers/Microsoft.Network/frontdoorwebapplicationfirewallpolicies/${replace(host.value["name"], "-", "")}${replace(var.env, "-", "")}"
+      web_application_firewall_policy_link_id = "/subscriptions/${var.subscription_id}/resourcegroups/${var.resource_group}/providers/Microsoft.Network/frontdoorWebApplicationFirewallPolicies/${replace(host.value["name"], "-", "")}${replace(var.env, "-", "")}"
       dynamic "custom_https_configuration" {
         for_each = lookup(host.value, "enable_ssl", true) ? [1] : []
         content {
