39ba115eeb43a45fb2b940cdfdb415acb4acfe74
mokainos <72137340+mokainos@users.noreply.github.com>
2020-11-11 10:15:35 +0000

    Update provider.tf

--- validation/provider.tf
+++ validation/provider.tf
@@ -6,8 +6,7 @@ provider "azurerm" {
 
 
 provider "azurerm" {
-  #skip_provider_registration = "true"
-  #version         = "= 2.4.0"
+
   features {}
   subscription_id = "b72ab7b7-723f-4b18-b6f6-03b0f2c6a1bb"
 }
