ed04289a01c6ca93afb883371d9244c3ed526b83
naikajah <piyush.m82@gmail.com>
2021-02-02 21:23:34 +0000

    Https redirect Rule

--- rulesengine.tf
+++ rulesengine.tf
@@ -12,3 +12,18 @@ resource "azurerm_template_deployment" "rules" {
 
   depends_on = [azurerm_frontdoor.main]
 }
+
+resource "null_resource" "rule_association" {
+  for_each = {
+    for frontend in var.frontends : frontend.name => frontend if lookup(host.value, "enable_ssl", true)
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+      az login --service-principal --username $clientId --password $secret --tenant $tenantId
+      az network front-door routing-rule update --front-door-name ${azurerm_frontdoor.main.name} --resource-group ${var.resource_group} --name ${each.value["name"]} --rules-engine ${var.project}${var.env}
+    EOF
+  }
+
+  depends_on = [azurerm_template_deployment.rules]
+}
