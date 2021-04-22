89a0e46f53850b85c46921ba221ca8fd5c6d9b65
Tim Jacomb <tim.jacomb@hmcts.net>
2021-04-22 09:54:46 +0100

    Switch to other resource

--- frontdoor.tf
+++ frontdoor.tf
@@ -8,7 +8,6 @@ resource "azurerm_frontdoor" "main" {
   frontend_endpoint {
     name                              = "${var.project}-${var.env}-azurefd-net"
     host_name                         = "${var.project}-${var.env}.azurefd.net"
-    custom_https_provisioning_enabled = false
   }
 
   backend_pool_load_balancing {
@@ -53,16 +52,7 @@ resource "azurerm_frontdoor" "main" {
     content {
       name                                    = host.value["name"]
       host_name                               = host.value["custom_domain"]
-      custom_https_provisioning_enabled       = lookup(host.value, "enable_ssl", true)
       web_application_firewall_policy_link_id = azurerm_frontdoor_firewall_policy.custom[host.value["name"]].id
-      dynamic "custom_https_configuration" {
-        for_each = lookup(host.value, "enable_ssl", true) ? [1] : []
-        content {
-          certificate_source                      = var.ssl_mode
-          azure_key_vault_certificate_vault_id    = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault.certificate_vault.id : null
-          azure_key_vault_certificate_secret_name = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault_secret.certificate[host.value["name"]].name : null
-        }
-      }
     }
   }
 
@@ -202,3 +192,20 @@ resource "azurerm_frontdoor" "main" {
 
   depends_on = [azurerm_frontdoor_firewall_policy.custom, azurerm_key_vault_access_policy.frontdoor_kv_access]
 }
+
+resource "azurerm_frontdoor_custom_https_configuration" "https" {
+  for_each = { for frontend in var.frontends :
+    frontend.name => frontend
+  }
+
+  frontend_endpoint_id              = azurerm_frontdoor.main.frontend_endpoint[each.value["name"]]
+  custom_https_provisioning_enabled = true
+
+  custom_https_configuration {
+    certificate_source                         = "AzureKeyVault"
+    azure_key_vault_certificate_secret_name    = data.azurerm_key_vault_secret.certificate[each.value["name"]].name
+    azure_key_vault_certificate_vault_id       = data.azurerm_key_vault.certificate_vault.id
+  }
+}
+
+// TODO www redirect
