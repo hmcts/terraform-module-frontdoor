42785d68aba2319ae51d2823bbe59c024f54a446
Tim Jacomb <tim.jacomb@hmcts.net>
2021-04-23 10:13:23 +0100

    Trying

--- frontdoor.tf
+++ frontdoor.tf
@@ -185,7 +185,7 @@ resource "azurerm_frontdoor" "main" {
 
 resource "azurerm_frontdoor_custom_https_configuration" "https" {
   for_each = toset([
-    for endpoint in azurerm_frontdoor.main.frontend_endpoints: endpoint if (replace(endpoint, "azurefd-net", "") != endpoint)
+    for endpoint in azurerm_frontdoor.main.frontend_endpoints: endpoint if endpoint != replace(endpoint, "azurefd-net", "")
   ])
 
   frontend_endpoint_id              = each.value
