980c4027e8e9352dc639ea7bc51576b181131355
Tim Jacomb <tim.jacomb@hmcts.net>
2021-05-20 13:42:26 +0100

    Allow specifying mode per frontend

--- frontdoor.tf
+++ frontdoor.tf
@@ -193,9 +193,10 @@ resource "azurerm_frontdoor_custom_https_configuration" "https" {
   custom_https_provisioning_enabled = true
 
   custom_https_configuration {
-    certificate_source                      = var.ssl_mode
-    azure_key_vault_certificate_secret_name = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault_secret.certificate[each.value["name"]].name : null
-    azure_key_vault_certificate_vault_id    = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault.certificate_vault.id : null
+    // apex domains aren't supported by managed mode, so we are keeping support for custom ssl
+    certificate_source                      = lookup(each.value, "ssl_mode", var.ssl_mode)
+    azure_key_vault_certificate_secret_name = lookup(each.value, "ssl_mode", var.ssl_mode) == "AzureKeyVault" ? data.azurerm_key_vault_secret.certificate[each.value["name"]].name : null
+    azure_key_vault_certificate_vault_id    = lookup(each.value, "ssl_mode", var.ssl_mode) == "AzureKeyVault" ? data.azurerm_key_vault.certificate_vault.id : null
   }
 
   depends_on = [azurerm_frontdoor.main]
@@ -211,8 +212,9 @@ resource "azurerm_frontdoor_custom_https_configuration" "https_www_redirect" {
   custom_https_provisioning_enabled = true
 
   custom_https_configuration {
-    certificate_source                      = var.ssl_mode
-    azure_key_vault_certificate_secret_name = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault_secret.certificate[each.value["name"]].name : null
-    azure_key_vault_certificate_vault_id    = var.ssl_mode == "AzureKeyVault" ? data.azurerm_key_vault.certificate_vault.id : null
+    // apex domains aren't supported by managed mode, so we are keeping support for custom ssl
+    certificate_source                      = lookup(each.value, "ssl_mode", var.ssl_mode)
+    azure_key_vault_certificate_secret_name = lookup(each.value, "ssl_mode", var.ssl_mode) == "AzureKeyVault" ? data.azurerm_key_vault_secret.certificate[each.value["name"]].name : null
+    azure_key_vault_certificate_vault_id    = lookup(each.value, "ssl_mode", var.ssl_mode) == "AzureKeyVault" ? data.azurerm_key_vault.certificate_vault.id : null
   }
 }
