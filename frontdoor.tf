6622111dc6f5264b1adc4b5b6e8eaf9a4dd5c27d
Enda Kelly <enda.kelly@hmcts.net>
2022-06-21 09:33:50 +0100

    update routing rule for cache_enabled

--- frontdoor.tf
+++ frontdoor.tf
@@ -128,9 +128,10 @@ resource "azurerm_frontdoor" "main" {
 
   dynamic "routing_rule" {
     iterator = host
-    for_each = [
-      for frontend in var.frontends : frontend if lookup(frontend, "cache_enabled", []) != [] ? true : false
-    ]
+    # for_each = [
+    #   for frontend in var.frontends : frontend if lookup(frontend, "cache_enabled", []) != [] ? true : false
+    # ]
+    for_each = var.frontends
     content {
       name               = host.value["name"]
       accepted_protocols = lookup(host.value, "enable_ssl", true) ? ["Https"] : ["Http"]
@@ -148,25 +149,25 @@ resource "azurerm_frontdoor" "main" {
     }
   }
 
-  dynamic "routing_rule" {
-    iterator = host
-    for_each = [
-      for frontend in var.frontends : frontend if lookup(frontend, "cache_enabled", false)
-    ]
-    content {
-      name               = host.value["name"]
-      accepted_protocols = lookup(host.value, "enable_ssl", true) ? ["Https"] : ["Http"]
-      patterns_to_match  = lookup(host.value, "url_patterns", ["/*"])
-      frontend_endpoints = [host.value["name"]]
+  # dynamic "routing_rule" {
+  #   iterator = host
+  #   for_each = [
+  #     for frontend in var.frontends : frontend if lookup(frontend, "cache_enabled", false)
+  #   ]
+  #   content {
+  #     name               = host.value["name"]
+  #     accepted_protocols = lookup(host.value, "enable_ssl", true) ? ["Https"] : ["Http"]
+  #     patterns_to_match  = lookup(host.value, "url_patterns", ["/*"])
+  #     frontend_endpoints = [host.value["name"]]
 
-      forwarding_configuration {
-        forwarding_protocol           = lookup(host.value, "forwarding_protocol", "HttpOnly")
-        backend_pool_name             = lookup(host.value, "backend_domain", []) == [] ? host.value["backend"] : host.value["name"]
-        cache_use_dynamic_compression = false
-        custom_forwarding_path        = ""
-      }
-    }
-  }
+  #     forwarding_configuration {
+  #       forwarding_protocol           = lookup(host.value, "forwarding_protocol", "HttpOnly")
+  #       backend_pool_name             = lookup(host.value, "backend_domain", []) == [] ? host.value["backend"] : host.value["name"]
+  #       cache_use_dynamic_compression = false
+  #       custom_forwarding_path        = ""
+  #     }
+  #   }
+  # }
 
   dynamic "routing_rule" {
     iterator = host
