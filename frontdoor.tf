8ced34974f3f29297599d24a788f67c1650f3a80
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2022-12-05 11:51:41 +0000

    try nullable argument

--- frontdoor.tf
+++ frontdoor.tf
@@ -124,20 +124,22 @@ resource "azurerm_frontdoor" "main" {
   }
 
   dynamic "routing_rule" {
+    # this routing rule will be added if cache_enabled is true
     iterator = host
-    for_each = var.frontends
-
+    for_each = [
+      for frontend in var.frontends : frontend if lookup(frontend, "cache_enabled", true)
+    ]
     content {
       name               = host.value["name"]
       accepted_protocols = lookup(host.value, "enable_ssl", true) ? ["Https"] : ["Http"]
       patterns_to_match  = lookup(host.value, "url_patterns", ["/*"])
       frontend_endpoints = [host.value["name"]]
 
-      dynamic "forwarding_configuration" {
+      forwarding_configuration {
         forwarding_protocol                   = lookup(host.value, "forwarding_protocol", "HttpOnly")
         backend_pool_name                     = lookup(host.value, "backend_domain", []) == [] ? host.value["backend"] : host.value["name"]
         cache_enabled                         = lookup(host.value, "cache_enabled", "true")
-        cache_query_parameter_strip_directive = try(lookup(host.value, "cache_enabled", "StripAll"), "StripNone")
+        cache_query_parameter_strip_directive = "StripNone" ? null : null
         cache_use_dynamic_compression         = false
         custom_forwarding_path                = ""
       }
