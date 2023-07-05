24c3715d67dcb1787467d1f98f67e10269d46666
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-07-05 15:51:22 +0100

    testing new frontdoor

--- diagnostics.tf
+++ diagnostics.tf
@@ -1,7 +1,7 @@
 
 resource "azurerm_monitor_diagnostic_setting" "frontdoor_diagnostics" {
   name                       = "fd-log-analytics"
-  target_resource_id         = azurerm_frontdoor.main.id
+  target_resource_id         = azurerm_cdn_frontdoor_profile.my_front_door.id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
   enabled_log {
