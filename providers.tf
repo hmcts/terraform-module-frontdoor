8454924c0769e25e8fe16101aec09ac0b917d220
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-09-07 14:54:35 +0100

    removing retention rules as they are deprecated

--- providers.tf
+++ providers.tf
@@ -3,7 +3,7 @@ terraform {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.34.0"
-      configuration_aliases = [ azurerm.public_dns ]
+      # configuration_aliases = [ azurerm.public_dns ]
     }
   }
 }
