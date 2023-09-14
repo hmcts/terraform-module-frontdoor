22b2db0df3343c7eb1a9d1fbf137c68bbb7a56af
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-09-14 15:28:56 +0100

    testing frontdoor

--- new_frontdoor.tf
+++ new_frontdoor.tf
@@ -218,22 +218,22 @@ resource "azurerm_cdn_frontdoor_custom_domain_association" "custom_association_D
   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.routing_rule_D[each.key].id]
 }
 
-data "azurerm_dns_zone" "public_dns" {
-  for_each            = { for frontend in var.new_frontends : frontend.name => frontend }
-  provider = azurerm.public_dns
-  name                = replace(each.value.custom_domain, "^[^.]+\\.", "")
-  resource_group_name = "reformmgmtrg"
-}
+# data "azurerm_dns_zone" "public_dns" {
+#   for_each            = { for frontend in var.new_frontends : frontend.name => frontend }
+#   provider = azurerm.public_dns
+#   name                = replace(each.value.custom_domain, "^[^.]+\\.", "")
+#   resource_group_name = "reformmgmtrg"
+# }
 
-resource "azurerm_dns_txt_record" "public_dns_record" {
-  for_each            = { for frontend in var.new_frontends : frontend.name => frontend }
-  provider            = azurerm.public_dns
-  name                = join(".", ["_dnsauth", each.value.custom_domain ])
-  zone_name           = data.azurerm_dns_zone.public_dns[each.key].name
-  resource_group_name = data.azurerm_dns_zone.public_dns[each.key].resource_group_name
-  ttl                 = 3600
+# resource "azurerm_dns_txt_record" "public_dns_record" {
+#   for_each            = { for frontend in var.new_frontends : frontend.name => frontend }
+#   provider            = azurerm.public_dns
+#   name                = join(".", ["_dnsauth", each.value.custom_domain ])
+#   zone_name           = data.azurerm_dns_zone.public_dns[each.key].name
+#   resource_group_name = data.azurerm_dns_zone.public_dns[each.key].resource_group_name
+#   ttl                 = 3600
 
-  record {
-    value = azurerm_cdn_frontdoor_custom_domain.custom_domain[each.key].validation_token
-  }
-}
+#   record {
+#     value = azurerm_cdn_frontdoor_custom_domain.custom_domain[each.key].validation_token
+#   }
+# }
