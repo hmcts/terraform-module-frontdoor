156bf48b273474707bf158c41179d8c07df9f084
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-09-29 19:34:31 +0100

    test

--- providers.tf
+++ providers.tf
@@ -5,5 +5,10 @@ terraform {
       version = ">= 3.34.0"
       configuration_aliases = [ azurerm.public_dns ]
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 1.0"
+      configuration_aliases = [ azapi.frontdoor_azapi ]
+    }
   }
 }
