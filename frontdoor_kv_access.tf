5b15def4206db793894957b4933b8dbda10dd3bc
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-09-29 09:44:47 +0100

    testing function

--- frontdoor_kv_access.tf
+++ frontdoor_kv_access.tf
@@ -1,14 +1,14 @@
-resource "azurerm_key_vault_access_policy" "frontdoor_kv_access" {
-  count        = var.add_access_policy == true ? 1 : 0
-  key_vault_id = data.azurerm_key_vault.certificate_vault.id
+# resource "azurerm_key_vault_access_policy" "frontdoor_kv_access" {
+#   count        = var.add_access_policy == true ? 1 : 0
+#   key_vault_id = data.azurerm_key_vault.certificate_vault.id
 
-  object_id = "0a7e9367-4349-4b24-974a-bfa7b23a38fc"
-  tenant_id = data.azurerm_client_config.current.tenant_id
+#   object_id = "0a7e9367-4349-4b24-974a-bfa7b23a38fc"
+#   tenant_id = data.azurerm_client_config.current.tenant_id
 
-  secret_permissions      = ["Get", "List"]
-  certificate_permissions = ["Get", "List"]
-  key_permissions         = ["Get", "List"]
-}
+#   secret_permissions      = ["Get", "List"]
+#   certificate_permissions = ["Get", "List"]
+#   key_permissions         = ["Get", "List"]
+# }
 
 resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
   count        = var.add_access_policy == true ? 1 : 0
