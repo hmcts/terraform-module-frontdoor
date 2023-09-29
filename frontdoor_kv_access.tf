08c2ce10d94e608bda202985b4ea5879de1e3fe4
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-09-29 11:10:14 +0100

    testing function

--- frontdoor_kv_access.tf
+++ frontdoor_kv_access.tf
@@ -30,10 +30,10 @@ resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
 #   scope                = data.azurerm_key_vault.certificate_vault.id
 # }
 
-resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
-  count = var.add_access_policy_role == true ? 1 : 0
+# resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
+#   count = var.add_access_policy_role == true ? 1 : 0
 
-  role_definition_name = "Key Vault Secrets User"
-  principal_id         = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
-  scope                = data.azurerm_key_vault.certificate_vault.id
-}
+#   role_definition_name = "Key Vault Secrets User"
+#   principal_id         = "205478c0-bd83-4e1b-a9d6-db63a3e1e1c8"
+#   scope                = data.azurerm_key_vault.certificate_vault.id
+# }
