faec4770e526a0f833c0591c181fc83748c7906e
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-10-02 15:16:22 +0100

    test

--- new_frontdoor.tf
+++ new_frontdoor.tf
@@ -257,21 +257,21 @@ resource "azurerm_cdn_frontdoor_custom_domain_association" "custom_association_D
   cdn_frontdoor_route_ids        = [azurerm_cdn_frontdoor_route.routing_rule_D[each.key].id]
 }
 
-data "azurerm_dns_zone" "public_dns_apex" {
+data "azurerm_dns_zone" "public_dns" {
   for_each = { for frontend in var.new_frontends : frontend.name => frontend
     if lookup(frontend, "is_apex", false) == true
   }
   provider            = azurerm.public_dns
-  name                = each.value.custom_domain
+  name                = each.value.is_apex ? each.value.custom_domain : replace(each.value.custom_domain, "/^[^.]+\\./", "")
   resource_group_name = "reformmgmtrg"
 }
 
-resource "azurerm_dns_txt_record" "public_dns_record_apex" {
+resource "azurerm_dns_txt_record" "public_dns_record" {
   for_each = var.add_txt_record ? { for frontend in var.new_frontends : frontend.name => frontend
     if lookup(frontend, "is_apex", false) == true
   } : {}
   provider            = azurerm.public_dns
-  name                = "_dnsauth"
+  name                = each.value.is_apex ? "_dnsauth" : join(".", ["_dnsauth", element(split(".", each.value.custom_domain), 0)])
   zone_name           = data.azurerm_dns_zone.public_dns_apex[each.key].name
   resource_group_name = data.azurerm_dns_zone.public_dns_apex[each.key].resource_group_name
   ttl                 = 3600
@@ -281,29 +281,29 @@ resource "azurerm_dns_txt_record" "public_dns_record_apex" {
   }
 }
 
-data "azurerm_dns_zone" "public_dns" {
-  for_each = { for frontend in var.new_frontends : frontend.name => frontend
-    if lookup(frontend, "is_apex", false) != true
-  }
-  provider            = azurerm.public_dns
-  name                = replace(each.value.custom_domain, "/^[^.]+\\./", "")
-  resource_group_name = "reformmgmtrg"
-}
+# data "azurerm_dns_zone" "public_dns" {
+#   for_each = { for frontend in var.new_frontends : frontend.name => frontend
+#     if lookup(frontend, "is_apex", false) != true
+#   }
+#   provider            = azurerm.public_dns
+#   name                = replace(each.value.custom_domain, "/^[^.]+\\./", "")
+#   resource_group_name = "reformmgmtrg"
+# }
 
-resource "azurerm_dns_txt_record" "public_dns_record" {
-  for_each = var.add_txt_record ? { for frontend in var.new_frontends : frontend.name => frontend
-    if lookup(frontend, "is_apex", false) != true
-  } : {}
-  provider            = azurerm.public_dns
-  name                = join(".", ["_dnsauth", element(split(".", each.value.custom_domain), 0)])
-  zone_name           = data.azurerm_dns_zone.public_dns[each.key].name
-  resource_group_name = data.azurerm_dns_zone.public_dns[each.key].resource_group_name
-  ttl                 = 3600
+# resource "azurerm_dns_txt_record" "public_dns_record" {
+#   for_each = var.add_txt_record ? { for frontend in var.new_frontends : frontend.name => frontend
+#     if lookup(frontend, "is_apex", false) != true
+#   } : {}
+#   provider            = azurerm.public_dns
+#   name                = join(".", ["_dnsauth", element(split(".", each.value.custom_domain), 0)])
+#   zone_name           = data.azurerm_dns_zone.public_dns[each.key].name
+#   resource_group_name = data.azurerm_dns_zone.public_dns[each.key].resource_group_name
+#   ttl                 = 3600
 
-  record {
-    value = azurerm_cdn_frontdoor_custom_domain.custom_domain[each.key].validation_token
-  }
-}
+#   record {
+#     value = azurerm_cdn_frontdoor_custom_domain.custom_domain[each.key].validation_token
+#   }
+# }
 
 resource "azurerm_dns_cname_record" "cname" {
   depends_on = [azurerm_cdn_frontdoor_route.routing_rule_A, azurerm_cdn_frontdoor_route.routing_rule_B, azurerm_cdn_frontdoor_route.routing_rule_C, azurerm_cdn_frontdoor_route.routing_rule_D, azurerm_cdn_frontdoor_security_policy.security_policy]
