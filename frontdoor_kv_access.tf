d34e847b8c7dffefcb6dc4b91dc58c77cd491b06
Tyler McDowell <tyler.mcdowell@justice.gov.uk>
2023-10-02 16:04:54 +0100

    test

--- frontdoor_kv_access.tf
+++ frontdoor_kv_access.tf
@@ -18,21 +18,21 @@
 #   scope                = data.azurerm_key_vault.certificate_vault.id
 # }
 
-# resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
-#   count        = var.add_access_policy == true ? 1 : 0
-#   key_vault_id = data.azurerm_key_vault.certificate_vault.id
+resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
+  count        = var.add_access_policy == true ? 1 : 0
+  key_vault_id = data.azurerm_key_vault.certificate_vault.id
 
-#   object_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
-#   tenant_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.tenantId
-#   secret_permissions      = ["Get", "List"]
-#   certificate_permissions = ["Get", "List"]
-#   key_permissions         = ["Get", "List"]
-# }
+  object_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
+  tenant_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.tenantId
+  secret_permissions      = ["Get", "List"]
+  certificate_permissions = ["Get", "List"]
+  key_permissions         = ["Get", "List"]
+}
 
-# resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
-#   count = var.add_access_policy_role == true ? 1 : 0
+resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
+  count = var.add_access_policy_role == true ? 1 : 0
 
-#   role_definition_name = "Key Vault Secrets User"
-#   principal_id         = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
-#   scope                = data.azurerm_key_vault.certificate_vault.id
-# }
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
+  scope                = data.azurerm_key_vault.certificate_vault.id
+}
