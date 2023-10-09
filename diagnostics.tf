523820c92f9acc304dacbf6823f74b130a3d212a
Tim Jacomb <timjacomb1@gmail.com>
2023-10-09 16:22:08 +0100

    Adjust FrontDoor diagnostics

--- diagnostics.tf
+++ diagnostics.tf
@@ -1,32 +1,37 @@
-
 resource "azurerm_monitor_diagnostic_setting" "frontdoor_diagnostics" {
   name                       = "fd-log-analytics"
   target_resource_id         = azurerm_frontdoor.main.id
   log_analytics_workspace_id = var.log_analytics_workspace_id
 
   enabled_log {
-    category = "FrontdoorAccessLog"
-
-    retention_policy {
-      enabled = true
-      days    = 7
-    }
-  }
-
-  enabled_log {
     category = "FrontdoorWebApplicationFirewallLog"
-    retention_policy {
-      enabled = true
-      days    = 30
-    }
   }
 
   metric {
     category = "AllMetrics"
+  }
+}
 
-    retention_policy {
-      enabled = true
-      days    = 30
-    }
+resource "azurerm_monitor_diagnostic_setting" "diagnostics_access_logs_la" {
+  count = var.send_access_logs_to_log_analytics ? 1 : 0
+
+  name                       = "fd-log-analytics-logs"
+  target_resource_id         = azurerm_frontdoor.main.id
+  log_analytics_workspace_id = var.log_analytics_workspace_id
+
+  enabled_log {
+    category = "FrontdoorAccessLog"
+  }
+}
+
+resource "azurerm_monitor_diagnostic_setting" "diagnostics_access_logs_sa" {
+  count = var.diagnostics_storage_account_id != null ? 1 : 0
+
+  name               = "fd-log-analytics-logs-sa"
+  target_resource_id = azurerm_frontdoor.main.id
+  storage_account_id = var.diagnostics_storage_account_id
+
+  enabled_log {
+    category = "FrontdoorAccessLog"
   }
 }
