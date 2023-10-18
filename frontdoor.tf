56dd68ef1270cc42a25b1c2531cf659761189b4c
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-10-18 12:58:38 +0100

    added missing host_header

--- frontdoor.tf
+++ frontdoor.tf
@@ -100,7 +100,7 @@ resource "azurerm_cdn_frontdoor_origin" "front_door_origin" {
   host_name                      = each.value.backend_domain[0]
   http_port                      = lookup(each.value, "http_port", 80)
   https_port                     = 443
-  origin_host_header             = each.value.custom_domain
+  origin_host_header             = lookup(each.value, "host_header", each.value.custom_domain)
   priority                       = 1
   weight                         = 50
   certificate_name_check_enabled = true
