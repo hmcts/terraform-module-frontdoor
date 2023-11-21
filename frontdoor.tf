32d8b9690970526350a3774fa20b33c151c7d9b8
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2023-11-21 08:20:22 +0000

    update if

--- frontdoor.tf
+++ frontdoor.tf
@@ -383,7 +383,7 @@ resource "azurerm_cdn_frontdoor_custom_domain_association" "custom_association_D
 
 data "azurerm_dns_zone" "public_dns" {
   for_each = { for frontend in var.frontends : frontend.name => frontend
-  if !contains(each.value.dns_zone_name, "dsd.io")
+  if lookup(frontend, "hosted_externally", null) == null
   }
   provider            = azurerm.public_dns
   name                = each.value.dns_zone_name
@@ -392,7 +392,7 @@ data "azurerm_dns_zone" "public_dns" {
 
 resource "azurerm_dns_txt_record" "public_dns_record" {
   for_each = { for frontend in var.frontends : frontend.name => frontend
-  if !contains(each.value.dns_zone_name, "dsd.io")
+  if lookup(frontend, "hosted_externally", null) == null
   }
   provider = azurerm.public_dns
   name = trimsuffix(
