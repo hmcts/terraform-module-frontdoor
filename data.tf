a3092fe7c031fff8b6ac42cb7e5b0a01b695e61c
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-11-29 14:52:43 +0000

    restored data

--- data.tf
+++ data.tf
@@ -1,9 +1,5 @@
 # KeyVault for the certificate must be in the same subscription as Frontdoor.
 data "azurerm_key_vault" "certificate_vault" {
-  for_each = { for frontend in var.frontends :
-    frontend.name => frontend
-    if lookup(frontend, "ssl_mode", var.ssl_mode) == "AzureKeyVault"
-  }
   name                = var.certificate_key_vault_name
   resource_group_name = var.key_vault_resource_group
 }
@@ -14,7 +10,7 @@ data "azurerm_key_vault_secret" "certificate" {
     if lookup(frontend, "ssl_mode", var.ssl_mode) == "AzureKeyVault"
   }
   name         = lookup(each.value, "certificate_name")
-  key_vault_id = data.azurerm_key_vault.certificate_vault[each.key].id
+  key_vault_id = data.azurerm_key_vault.certificate_vault.id
 }
 
 data "azurerm_key_vault_certificate" "certificate" {
@@ -23,7 +19,7 @@ data "azurerm_key_vault_certificate" "certificate" {
     if lookup(frontend, "ssl_mode", var.ssl_mode) == "AzureKeyVault"
   }
   name         = lookup(each.value, "certificate_name")
-  key_vault_id = data.azurerm_key_vault.certificate_vault[each.key].id
+  key_vault_id = data.azurerm_key_vault.certificate_vault.id
 }
 
 data "azurerm_client_config" "current" {}
