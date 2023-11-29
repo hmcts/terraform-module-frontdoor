83f1545e8c7b8ef6abb43fac5045869cf8b59992
Chirag Pareek <chirag.pareek@justice.gov.uk>
2023-11-29 14:57:14 +0000

    fix

--- frontdoor_kv_access.tf
+++ frontdoor_kv_access.tf
@@ -1,6 +1,6 @@
 resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
   count        = var.add_access_policy == true ? 1 : 0
-  key_vault_id = data.azurerm_key_vault.certificate_vault[each.key].id
+  key_vault_id = data.azurerm_key_vault.certificate_vault.id
 
   object_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
   tenant_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.tenantId
@@ -14,5 +14,5 @@ resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
 
   role_definition_name = "Key Vault Secrets User"
   principal_id         = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
-  scope                = data.azurerm_key_vault.certificate_vault[each.key].id
+  scope                = data.azurerm_key_vault.certificate_vault.id
 }
