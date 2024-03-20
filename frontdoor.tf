30ec7a5cd3902ef2fdca6d808f8b05af386b7749
Ciarán Carragher <ciaran.carragher2@justice.gov.uk>
2024-03-20 14:46:00 +0000

    change to dynamic blcok

--- frontdoor.tf
+++ frontdoor.tf
@@ -112,24 +112,26 @@ resource "azurerm_cdn_frontdoor_origin" "front_door_origin" {
   priority                       = 1
   weight                         = 50
   certificate_name_check_enabled = lookup(each.value, "certificate_name_check_enabled", true) ? true : false
+
+  dynamic "azurerm_cdn_frontdoor_origin" {
+    for_each = { for frontend in var.frontends : frontend.name => frontend
+    if length(frontend.backend_domain) > 1 ? true : false }
+
+    content {
+      name                          = lookup(each.value, "origin_group_name", each.value.name)
+      cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group[each.key].id
+
+      enabled                        = true
+      host_name                      = each.value.backend_domain[1]
+      http_port                      = lookup(each.value, "http_port", 80)
+      https_port                     = 443
+      origin_host_header             = lookup(each.value, "host_header", each.value.custom_domain)
+      priority                       = 2
+      weight                         = 50
+      certificate_name_check_enabled = lookup(each.value, "certificate_name_check_enabled", true) ? true : false
+    }
+  }
 }
-
-resource "azurerm_cdn_frontdoor_origin" "front_door_origin_2" {
-  for_each = { for frontend in var.frontends : frontend.name => frontend
-  if length(frontend.backend_domain) ==2 ? true : false }
-  name                          = lookup(each.value, "origin_group_name", each.value.name)
-  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.origin_group[each.key].id
-
-  enabled                        = true
-  host_name                      = each.value.backend_domain[1]
-  http_port                      = lookup(each.value, "http_port", 80)
-  https_port                     = 443
-  origin_host_header             = lookup(each.value, "host_header", each.value.custom_domain)
-  priority                       = 2
-  weight                         = 50
-  certificate_name_check_enabled = lookup(each.value, "certificate_name_check_enabled", true) ? true : false
-}
-
 
 resource "azurerm_cdn_frontdoor_route" "routing_rule_A" {
   for_each = {
