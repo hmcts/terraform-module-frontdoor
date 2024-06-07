2c412d9ce495d9fe302877fae283c745cc1390fa
Chirag Pareek <chirag.pareek@justice.gov.uk>
2024-06-07 10:26:54 +0100

    testing setting default values for route_configuration_override_action

--- rules.tf
+++ rules.tf
@@ -56,13 +56,13 @@ resource "azurerm_cdn_frontdoor_rule" "cache_static_rule" {
       for_each = each.value.cache_static_files.route_configuration_override_action
       iterator = action
       content {
-        cache_duration                = action.value.cache_duration
-        cdn_frontdoor_origin_group_id = action.value.cdn_frontdoor_origin_group_id
-        forwarding_protocol           = action.value.forwarding_protocol
-        query_string_caching_behavior = action.value.query_string_caching_behavior
-        query_string_parameters       = action.value.query_string_parameters
-        compression_enabled           = action.value.compression_enabled
-        cache_behavior                = action.value.cache_behavior
+        cache_duration                = lookup(action.value, "cache_duration", null) != null ? action.value.cache_duration : null
+        cdn_frontdoor_origin_group_id = lookup(action.value, "cdn_frontdoor_origin_group_id", null) != null ? action.value.cdn_frontdoor_origin_group_id : null
+        forwarding_protocol           = lookup(action.value, "forwarding_protocol", null) != null ? action.value.forwarding_protocol : null
+        query_string_caching_behavior = lookup(action.value, "query_string_caching_behavior", null) != null ? action.value.query_string_caching_behavior : "UseQueryString"
+        query_string_parameters       = lookup(action.value, "query_string_parameters", null) != null ? action.value.query_string_parameters : null
+        compression_enabled           = lookup(action.value, "compression_enabled", null) != null ? action.value.compression_enabled : false
+        cache_behavior                = lookup(action.value, "cache_behavior", null) != null ? action.value.cache_behavior : "HonorOrigin"
       }
     }
   }
