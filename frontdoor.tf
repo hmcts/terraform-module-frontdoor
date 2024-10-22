fbe11938baaf7b0e50694e5d68187d6d1f122e56
Alex Bance <abance@bancey.xyz>
2024-10-22 12:24:14 +0100

    fix: update az api cdn profile api versions

--- frontdoor.tf
+++ frontdoor.tf
@@ -6,7 +6,7 @@ resource "azurerm_cdn_frontdoor_profile" "front_door" {
 }
 
 resource "azapi_update_resource" "frontdoor_system_identity" {
-  type        = "Microsoft.Cdn/profiles@2023-02-01-preview"
+  type        = "Microsoft.Cdn/profiles@2023-07-01-preview"
   resource_id = azurerm_cdn_frontdoor_profile.front_door.id
   body = jsonencode({
     "identity" : {
