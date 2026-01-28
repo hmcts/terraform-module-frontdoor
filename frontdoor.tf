e8d6491087fc0fc8d918a4716f6861ef59ffbdd8
Rees Pozzi <reespozzi@icloud.com>
2026-01-28 12:12:43 +0000

    Try to use supported identity block

--- frontdoor.tf
+++ frontdoor.tf
@@ -3,18 +3,10 @@ resource "azurerm_cdn_frontdoor_profile" "front_door" {
   resource_group_name = var.resource_group
   sku_name            = var.front_door_sku_name
   tags                = var.common_tags
-}
 
-resource "azapi_update_resource" "frontdoor_system_identity" {
-  type        = "Microsoft.Cdn/profiles@2024-02-01"
-  resource_id = azurerm_cdn_frontdoor_profile.front_door.id
-  body = jsonencode({
-    "identity" : {
-      "type" : "SystemAssigned"
-    }
-  })
-  response_export_values = ["identity.principalId", "identity.tenantId"]
-
+  identity {
+    type = "SystemAssigned"
+  }
 }
 
 
--- frontdoor_kv_access.tf
+++ frontdoor_kv_access.tf
@@ -2,8 +2,8 @@ resource "azurerm_key_vault_access_policy" "frontdoor_premium_kv_access" {
   count        = var.add_access_policy == true ? 1 : 0
   key_vault_id = data.azurerm_key_vault.certificate_vault[0].id
 
-  object_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
-  tenant_id               = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.tenantId
+  object_id               = azurerm_cdn_frontdoor_profile.front_door.identity[0].principal_id
+  tenant_id               = azurerm_cdn_frontdoor_profile.front_door.identity[0].tenant_id
   secret_permissions      = ["Get", "List"]
   certificate_permissions = ["Get", "List"]
   key_permissions         = ["Get", "List"]
@@ -13,6 +13,6 @@ resource "azurerm_role_assignment" "frontdoor_premium_kv_access" {
   count = var.add_access_policy_role == true ? 1 : 0
 
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = jsondecode(azapi_update_resource.frontdoor_system_identity.output).identity.principalId
+  principal_id         = azurerm_cdn_frontdoor_profile.front_door.identity[0].principal_id
   scope                = data.azurerm_key_vault.certificate_vault[0].id
 }
